b765da3c285fe724918a980f3775d46deb40d7ed
Gerald Venzl <gerald.venzl@gmail.com>
2020-05-23 21:38:54 -0700

    Update uninstall
    Signed-off-by: Gerald Venzl <gerald.venzl@gmail.com>

---
+++ countries-and-cities/9_uninstall.sql
@@ -0,0 +1,26 @@
+/*
+ * Since: March, 2020
+ * Author: gvenzl
+ * Name: uninstall.sql
+ * Description: Uninstall (drop tables) script
+ *
+ * Copyright 2020 Gerald Venzl
+ *
+ * Provided with the generous help of my gorgeous wife Federica Venzl during the
+ * Coronavirus (COVID-19) pandemic.
+ *
+ * This work is licensed under the
+ * Creative Commons Attribution 4.0 International Public License, CC BY 4.0
+ *
+ *    https://creativecommons.org/licenses/by/4.0/
+ */
+
+DROP TABLE currencies_countries;
+
+DROP TABLE currencies;
+
+DROP TABLE cities;
+
+DROP TABLE countries;
+
+DROP TABLE regions;
