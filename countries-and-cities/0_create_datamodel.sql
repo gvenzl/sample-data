fa130dea7d4a92add1e4b5eea61fbd25e4e8fd1f
Gerald Venzl <gerald.venzl@gmail.com>
2020-05-23 21:37:51 -0700

    Update data model and header
    Signed-off-by: Gerald Venzl <gerald.venzl@gmail.com>

--- countries-and-cities/0_create_datamodel.sql
+++ countries-and-cities/0_create_datamodel.sql
@@ -6,17 +6,11 @@
  *
  * Copyright 2020 Gerald Venzl
  *
+ * Provided with the generous help of my gorgeous wife Federica Venzl during the
+ * Coronavirus (COVID-19) pandemic.
+ *
  * This work is licensed under the
  * Creative Commons Attribution 4.0 International Public License, CC BY 4.0
- *
- * Data Sources:
- * - GeoNames (https://www.geonames.org/)
- * - National Geographic Atlas of the World, Eleventh Edition, October 2019
- *
- * Provided with the generous help of my gorgeous wife Federica Venzl.
- *
- * The Data is provided "as is" without warranty or any representation of
- * accuracy, timeliness or completeness.
  *
  *    https://creativecommons.org/licenses/by/4.0/
  */ 
@@ -27,15 +21,8 @@
 
 CREATE TABLE regions
 (
-  region_id     VARCHAR(2)   NOT NULL,
+  region_id     VARCHAR(2)   NOT NULL PRIMARY KEY,
   name          VARCHAR(13)  NOT NULL
-);
-
-CREATE UNIQUE INDEX regions_pk ON regions (region_id);
-
-ALTER TABLE regions ADD
-(
-  CONSTRAINT regions_pk PRIMARY KEY (region_id)
 );
 
 /*********************************************/
@@ -44,26 +31,21 @@ ALTER TABLE regions ADD
 
 CREATE TABLE countries
 (
-  country_id    VARCHAR(3)    NOT NULL,
+  country_id    VARCHAR(3)    NOT NULL PRIMARY KEY,
   country_code  VARCHAR(2)    NOT NULL,
   name          VARCHAR(100)  NOT NULL,
   official_name VARCHAR(200),
-  latitude      NUMBER(8,5),
-  longitude     NUMBER(8,5),
-  population    INTEGER,
+  latitude      NUMERIC(8,5),
+  longitude     NUMERIC(8,5),
+  population    NUMERIC(10),
   area_sq_km    INTEGER,
   timezone      VARCHAR(40),
-  region_id     VARCHAR(2)    NOT NULL
-);
-
-CREATE UNIQUE INDEX countries_pk ON countries (country_id);
-CREATE INDEX countries_fk_idx001 ON countries (region_id);
-
-ALTER TABLE countries ADD
-(
-  CONSTRAINT countries_pk PRIMARY KEY (country_id),
+  region_id     VARCHAR(2)    NOT NULL,
+  
   CONSTRAINT countries_regions_fk001 FOREIGN KEY (region_id) REFERENCES regions (region_id)
 );
+
+CREATE INDEX countries_fk_idx001 ON countries (region_id);
 
 /*********************************************/
 /***************** CITIES ********************/
@@ -71,26 +53,21 @@ ALTER TABLE countries ADD
 
 CREATE TABLE cities
 (
-  city_id       INTEGER       NOT NULL,
+  city_id       VARCHAR(7)    NOT NULL PRIMARY KEY,
   name          VARCHAR(100)  NOT NULL,
   official_name VARCHAR(200),
-  latitude      NUMBER(8,5),
-  longitude     NUMBER(8,5),
-  population    INTEGER,
+  latitude      NUMERIC(8,5),
+  longitude     NUMERIC(8,5),
+  population    NUMERIC(8),
   is_capital    CHAR(1)       NOT NULL,
   timezone      VARCHAR(40),
-  country_id    VARCHAR(3)    NOT NULL
-);
-
-CREATE UNIQUE INDEX cities_pk ON cities (city_id);
-CREATE INDEX cities_fk_idx001 ON cities (country_id);
-
-ALTER TABLE cities ADD
-(
-  CONSTRAINT cities_pk PRIMARY KEY (city_id),
+  country_id    VARCHAR(3)    NOT NULL,
+  
   CONSTRAINT cities_countries_fk001 FOREIGN KEY (country_id) REFERENCES countries (country_id),
   CONSTRAINT cities_is_capital_Y_N_check001 CHECK (is_capital IN ('Y','N'))
 );
+
+CREATE INDEX cities_fk_idx001 ON cities (country_id);
 
 /*********************************************/
 /***************** CURRENCIES ****************/
@@ -98,17 +75,10 @@ ALTER TABLE cities ADD
 
 CREATE TABLE currencies
 (
-  currency_id       VARCHAR(3)    NOT NULL,
+  currency_id       VARCHAR(3)    NOT NULL PRIMARY KEY,
   name              VARCHAR(50)   NOT NULL,
   official_name     VARCHAR(200),
   symbol            VARCHAR(6)    NOT NULL
-);
-
-CREATE UNIQUE INDEX currencies_pk ON currencies (currency_id);
-
-ALTER TABLE	currencies ADD
-(
-  CONSTRAINT currencies_pk PRIMARY KEY (currency_id)
 );
 
 /*********************************************/
@@ -118,14 +88,9 @@ ALTER TABLE	currencies ADD
 CREATE TABLE currencies_countries
 (
   currency_id    VARCHAR(3)   NOT NULL,
-  country_id     VARCHAR(3)   NOT NULL
-);
-
-CREATE UNIQUE INDEX currencies_countries_pk ON currencies_countries (currency_id, country_id);
-
-ALTER TABLE	currencies_countries ADD
-(
-  CONSTRAINT currencies_countries_pk PRIMARY KEY (currency_id, country_id),
+  country_id     VARCHAR(3)   NOT NULL,
+  PRIMARY KEY (currency_id, country_id),
+  
   CONSTRAINT currencies_countries_currencies_fk001 FOREIGN KEY (currency_id) REFERENCES currencies (currency_id),
   CONSTRAINT currencies_countries_countries_fk002  FOREIGN KEY (country_id)  REFERENCES countries(country_id)
 );
