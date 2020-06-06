837904526a9e1e20ba6f2e08d35d29bcfcf05794
Gerald Venzl <gerald.venzl@gmail.com>
2020-06-06 12:13:21 -0700

    Unify install for all DBs
    Signed-off-by: Gerald Venzl <gerald.venzl@gmail.com>

--- departments-and-employees/install.oracle.sql
+++ departments-and-employees/install.oracle.sql
@@ -1,4 +1,5 @@
 /*
+ * Encoding: UTF-8
  * Since: May, 2020
  * Author: gvenzl
  * Name: install.oracle.sql
@@ -18,68 +19,8 @@
  *    https://creativecommons.org/licenses/by/4.0/
  */ 
 
-/*********************************************/
-/*************** DEPARTMENTS *****************/
-/*********************************************/
+-- Set DATE format
+ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD';
 
-CREATE TABLE departments
-(
-  department_id   SMALLINT      NOT NULL  CONSTRAINT departments_pk PRIMARY KEY,
-  name            VARCHAR(10)   NOT NULL,
-  location        VARCHAR(8)    NOT NULL
-);
-
-INSERT INTO departments (department_id, name, location) VALUES (1, 'Operations', 'Vienna');
-INSERT INTO departments (department_id, name, location) VALUES (2, 'Sales', 'New York');
-INSERT INTO departments (department_id, name, location) VALUES (3, 'Marketing', 'Paris');
-INSERT INTO departments (department_id, name, location) VALUES (4, 'Accounting', 'London');
-
-/*********************************************/
-/**************** EMPLOYEES ******************/
-/*********************************************/
-
-CREATE TABLE employees
-(
-  employee_id     SMALLINT      NOT NULL  CONSTRAINT employees_pk PRIMARY KEY,
-  first_name      VARCHAR(8),
-  last_name       VARCHAR(9)    NOT NULL,
-  job_title       VARCHAR(9),
-  manager_id      SMALLINT                CONSTRAINT employees_employees_fk001
-                                          REFERENCES employees (employee_id),
-  hire_date       DATE          NOT NULL,
-  salary          NUMERIC(7,2)  NOT NULL,
-  commission      NUMERIC(7,2),
-  department_id   SMALLINT      NOT NULL  CONSTRAINT employees_departments_fk001
-                                          REFERENCES departments (department_id)
-);
-
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1238, 'John', 'King', 'President', null, TO_DATE('2015-05-14','YYYY-MM-DD'), 10000, null, 1);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1346, 'Greg', 'Edelstein', 'Manager', 1238, TO_DATE('2016-06-17','YYYY-MM-DD'), 8000, null, 2);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1387, 'Erin', 'Smith', 'Manager', 1238, TO_DATE('2017-02-04','YYYY-MM-DD'), 6000, null, 3);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1362, 'Andrew', 'Counters', 'Analyst', 1238, TO_DATE('2016-11-03','YYYY-MM-DD'), 3000, null, 4);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1565, 'Sarah', 'Bean', 'Analyst', 1238, TO_DATE('2019-10-22','YYYY-MM-DD'), 2000, null, 4);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1456, 'James', 'Greed', 'Manager', 1346, TO_DATE('2018-03-12','YYYY-MM-DD'), 5000, null, 2);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1541, 'Judy', 'Mason', 'Manager', 1346, TO_DATE('2019-05-23','YYYY-MM-DD'), 4450, null, 2);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1257, 'Joseph', 'Maka', 'Sales Rep', 1456, TO_DATE('2015-05-20','YYYY-MM-DD'), 6000, 1000, 2);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1317, 'Graham', 'Cash', 'Sales Rep', 1456, TO_DATE('2015-12-08','YYYY-MM-DD'), 3000, 1500, 2);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1423, 'Amanda', 'Tunsa', 'Sales Rep', 1456, TO_DATE('2018-02-12','YYYY-MM-DD'), 3000, 1000, 2);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1512, 'Alastair', 'Walker', 'Sales Rep', 1541, TO_DATE('2018-07-24','YYYY-MM-DD'), 3000, 750, 2);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1603, 'Gerald', 'Venzl', 'Sales Rep', 1541, TO_DATE('2020-04-02','YYYY-MM-DD'), 2000, 500, 2);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1394, 'Monica', 'Point', 'Associate', 1387, TO_DATE('2017-05-13','YYYY-MM-DD'), 1200, null, 3);
-INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
-  VALUES (1404, 'Mark', 'Powers', 'Associate', 1387, TO_DATE('2018-01-14','YYYY-MM-DD'), 1000, null, 3);
-
-COMMIT;
+-- Call install script
+@install.sql
--- departments-and-employees/install.sql
+++ departments-and-employees/install.sql
@@ -1,8 +1,9 @@
 /*
+ * Encoding: UTF-8
  * Since: May, 2020
  * Author: gvenzl
  * Name: install.sql
- * Description: Setup script for departments and employees for PostgreSQL, SQL Server, Db2
+ * Description: Setup script for departments and employees
  *
  * Copyright 2020 Gerald Venzl
  *
@@ -24,9 +25,10 @@
 
 CREATE TABLE departments
 (
-  department_id   SMALLINT      NOT NULL  CONSTRAINT departments_pk PRIMARY KEY,
+  department_id   SMALLINT      NOT NULL,
   name            VARCHAR(10)   NOT NULL,
-  location        VARCHAR(8)    NOT NULL
+  location        VARCHAR(8)    NOT NULL,
+  CONSTRAINT departments_pk PRIMARY KEY (department_id)
 );
 
 INSERT INTO departments (department_id, name, location) VALUES (1, 'Operations', 'Vienna');
@@ -40,17 +42,21 @@ INSERT INTO departments (department_id, name, location) VALUES (4, 'Accounting',
 
 CREATE TABLE employees
 (
-  employee_id     SMALLINT      NOT NULL  CONSTRAINT employees_pk PRIMARY KEY,
+  employee_id     SMALLINT      NOT NULL,
   first_name      VARCHAR(8),
   last_name       VARCHAR(9)    NOT NULL,
   job_title       VARCHAR(9),
-  manager_id      SMALLINT                CONSTRAINT employees_employees_fk001
-                                          REFERENCES employees (employee_id),
+  manager_id      SMALLINT,
   hire_date       DATE          NOT NULL,
   salary          NUMERIC(7,2)  NOT NULL,
   commission      NUMERIC(7,2),
-  department_id   SMALLINT      NOT NULL  CONSTRAINT employees_departments_fk001
-                                          REFERENCES departments (department_id)
+  department_id   SMALLINT      NOT NULL,
+  CONSTRAINT employees_pk
+    PRIMARY KEY (employee_id),
+  CONSTRAINT employees_employees_fk001
+    FOREIGN KEY (manager_id) REFERENCES employees (employee_id),
+  CONSTRAINT employees_departments_fk001
+    FOREIGN KEY (department_id) REFERENCES departments (department_id)
 );
 
 INSERT INTO employees (employee_id, first_name, last_name, job_title, manager_id, hire_date, salary, commission, department_id)
